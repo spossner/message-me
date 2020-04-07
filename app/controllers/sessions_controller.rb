219f70bd25d08f34797860a0202b222c43f05f1e
Sebastian Possner <sebastian.possner@brands4friends.de>
2020-04-07 20:21:14 +0200

    login form and root pat

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    session[:user_id] = 1
+    flash[:success] = "Successfully logged in"
+    redirect_to root_path
+  end
 
+  def destroy
+    reset_session
+    flash[:success] = "Successfully logged out"
+    redirect_to root_path
   end
 end
